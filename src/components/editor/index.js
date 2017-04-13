5ba645abf6d2c3d52917252ad590544ee259abc1
ximing <ximing@meituan.com>
2017-04-13 12:35:49 +0800

    高度问题

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -1,28 +1,31 @@
 import './index.scss';
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import CodeMirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/javascript/javascript.js';
-export default class Editor extends Component{
-    
-    componentDidMount(){
+export default class Editor extends Component {
+
+    componentDidMount() {
         const codeMirrorDom = ReactDOM.findDOMNode(this.refs.editor);
-        this.codeMirror = CodeMirror(codeMirrorDom,{
-             mode:  "javascript",
-             lineNumbers:true,
-             lineWrapping:true,
-             inputStyle:'textarea',
-             
+        this.codeMirror = CodeMirror(codeMirrorDom, {
+            mode: "javascript",
+            lineNumbers: true,
+            lineWrapping: true,
+            inputStyle: 'textarea',
+            viewportMargin: Infinity,
+            maxHighlightLength: Infinity
         });
     }
-    
-    shouldComponentUpdate(){
+
+    shouldComponentUpdate() {
         return false;
     }
-    render(){
-        return(
-            <div ref="editor" className="ceditor-codemirror-container">
+
+    render() {
+        return (
+            <div ref="editor" className="content-container">
+                {/*<div className="CodeMirror"></div>*/}
             </div>
         )
     }
--- src/header.js
+++ src/header.js
@@ -1,10 +1,41 @@
 'use strict';
 import React,{Component} from 'react';
 export default class CEditor extends Component{
+    static defaultProps =  {
+        rightContent:null,
+        doc:{
+            name:'未命名'
+        }
+    }
+
+    backList = ()=>{}
+
     render(){
         return(
             <header className="ceditor-header">
-            ss
+                <div className="header-left-box list-header">
+                    <div className="s-header">
+                        <a className="header-back-up" onClick={this.backList}>
+                            <span className="header-back-icon"/>
+                        </a>
+                        <span className="s-header-text">
+                            <div className="span-input-wrap">
+                                <input className="title-input span-input" defaultValue={'ceshi.doc'} maxLength="100"
+                                       style={{
+                                           display: 'none'
+                                       }}/>
+                                <span className="title-input-pre span-input-pre">{this.props.doc.name || '未命名'}</span>
+                            </div>
+                        </span>
+                        <span className="s-header-time"
+                              id="save-status">{this.props.doc.status}</span>
+                    </div>
+                </div>
+                <div className="header-right-box">
+                    {this.props.rightContent}
+                </div>
+                <div className="editor-toolbar" id="toolbar">
+                </div>
             </header>
         )
     }
